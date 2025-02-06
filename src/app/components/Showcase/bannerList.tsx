80d81e0f2d2d6d44a5174d7fbff061824c03a1a0
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2025-02-06 14:38:11 +0530

    updates

--- src/app/components/Showcase/bannerList.tsx
+++ src/app/components/Showcase/bannerList.tsx
@@ -9,6 +9,8 @@ const data = [
     label: "MDFA",
     width: 116.568,
     height: 111.217,
+    widthM: 58.602,
+    heightM: 55.912,
     description1: "Currently playing",
     description2: "u-11 category",
   },
@@ -17,6 +19,8 @@ const data = [
     label: "PDFA",
     width: 200,
     height: 150,
+    widthM: 58.602,
+    heightM: 55.912,
     description1: "This page with",
     description2: "your stakeholders",
   },
@@ -28,26 +32,29 @@ const BannerList = () => {
       <div className="flex w-max animate-scroll">
         {[...data, ...data, ...data].map((item, index) => (
           <div
-            className="flex justify-center self-center w-full items-center h-[187.967px]  gap-10"
+            className="flex justify-center self-center w-full items-center lg:h-[187.967px] h-[94.496px] gap-10"
             key={index}
           >
             <div className="flex gap-10 px-10 items-center h-[187.967px] ">
-              <Image
-                src={item.image}
-                alt={item.label}
-                width={item.width}
-                height={item.height}
-                className="self-center"
-              />
-              <p className="text-[156.649px] text text-[#020037] font-schabo font-normal uppercase pt-4">
+              <div className="relative w-full h-auto">
+                <Image
+                  src={item.image}
+                  alt={item.label}
+                  width={item.width}
+                  height={item.height}
+                  className="self-center p-6 lg:p-0"
+                />
+              </div>
+
+              <p className="lg:text-[156.649px] text-[78.751px] text-[#020037] font-schabo font-normal uppercase pt-4">
                 {item.label}
               </p>
             </div>
-            <div className="flex flex-col justify-start">
-              <p className="text-[62.659px] leading-[56.394px] font-comedik font-normal text-[#020037] text-right self-center">
+            <div className="flex flex-col ">
+              <p className="lg:text-[62.659px] text-[31.501px] leading-[56.394px] font-comedik font-normal text-[#020037] text-right self-center">
                 {item.description1}
               </p>
-              <p className="text-[62.659px] leading-[56.394px] font-comedik font-normal text-[#020037] text-right self-center">
+              <p className="lg:text-[62.659px] text-[31.501px] leading-[56.394px] font-comedik font-normal text-[#020037] text-right self-center">
                 {item.description2}
               </p>
             </div>
